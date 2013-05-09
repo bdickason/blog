f468b36c0d8c37ddccf6e6140a8479e9b7b5ccc5
bdickason <dickason@gmail.com>
2013-05-09 01:00:03 -0400

    Removed restriction on single posts not displaying a color headline to liven things up

--- content.php
+++ content.php
@@ -16,13 +16,10 @@
 		<?php endif; ?>
 		<header class="entry-header">
 			<?php the_post_thumbnail(); ?>
-			<?php if ( is_single() ) : ?>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-			<?php else : ?>
+			
 			<h1 class="entry-title">
 				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 			</h1>
-			<?php endif; // is_single() ?>
 		</header><!-- .entry-header -->
 
 		<?php if ( is_search() ) : // Only display Excerpts for Search ?>
